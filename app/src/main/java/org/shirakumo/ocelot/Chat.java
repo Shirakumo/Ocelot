44d508bf46db7de67c9819935c6ad8563933a952
Shinmera <shinmera@tymoon.eu>
2017-12-01 19:31:23 +0100

    ゆっくりしていってね！！！

---
+++ app/src/main/java/org/shirakumo/ocelot/Chat.java
@@ -0,0 +1,13 @@
+package org.shirakumo.ocelot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Chat extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
